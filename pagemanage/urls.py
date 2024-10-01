8a1a5071497b366a3cf9470b54f28c8ae5a469b7
kantara <tan.kara24@gmail.com>
2024-10-01 13:44:26 +0300

    İndex sayfasının front endi tamamlandı

--- pagemanage/urls.py
+++ pagemanage/urls.py
@@ -4,4 +4,11 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="index"),
+    path('hizmetlerimiz', views.services),
+    path('hakkimizda', views.about),
+    path('iletisim', views.contact),
+    path('sss', views.sss),
+
+    
+
 ]
--- pagemanage/views.py
+++ pagemanage/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 def index(request):
     return render(request,'index.html')
 
+def services(request):
+    return render(request,'hizmetlerimiz.html')
+
+def about(request):
+    return render(request,'hakkimizda.html')
+
+def contact(request):
+    return render(request,'iletisim.html')
+
+def sss(request):
+    return render(request,'sss.html')
