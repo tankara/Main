047b63d48cba1aeb2e7a5ba0004aaab034be4c4b
kantara <tan.kara24@gmail.com>
2024-10-01 14:14:50 +0300

    model sınıfı düzenlendi

--- pagemanage/models.py
+++ pagemanage/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class People(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=20)
+    age = models.IntegerField()
+
+class Students(People):
+    grade = models.IntegerField()
+
+class Teachers(People):
+    branch = models.CharField(max_length=30)
+    numberOfClasses = models.IntegerField()
